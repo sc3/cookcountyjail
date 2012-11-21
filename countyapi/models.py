121f441f6c732b95aaaf368ac32ad434856b5b4b
David Eads <davideads@gmail.com>
2012-11-21 00:12:27 -0600

    update unicode on model

--- countyapi/models.py
+++ countyapi/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class CountyInmate(models.Model):
-    jail_id=models.CharField(max_length=12, primary_key=True)
-    last_name=models.CharField(max_length=100)
-    first_name=models.CharField(max_length=100)
+    jail_id=models.CharField(max_length=16, primary_key=True)
+    last_name=models.CharField(max_length=255)
+    first_name=models.CharField(max_length=255)
     # @TODO Add other fields
     # See e.g. http://www2.cookcountysheriff.org/search2/details.asp?jailnumber=2012-0813098 
+
+    def __unicode__(self):
+        return "%s, %s (%s)" % (self.last_name, self.first_name, self.jail_id)
