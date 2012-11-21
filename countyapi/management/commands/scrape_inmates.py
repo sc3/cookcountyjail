2ab1107bc09c8e792eb5ef405e0bfe76b8500439
David Eads <davideads@gmail.com>
2012-11-20 23:44:47 -0600

    update jail id

--- countyapi/management/commands/scrape_inmates.py
+++ countyapi/management/commands/scrape_inmates.py
@@ -2,18 +2,47 @@ import logging
 import requests
 from django.core.management.base import BaseCommand
 from pyquery import PyQuery as pq
+import string
+from countyapi.models import CountyInmate
 
-log = logging.getLogger('countyapi.management.commands.scrape_inmates')
+BASE_URL = "http://www2.cookcountysheriff.org/search2/"
 
 class Command(BaseCommand):
     help = "Scrape inmate data from Cook County Sheriff's site."
     def handle(self, *args, **kwargs):
         # @TODO scrape and load
         # Search, A-Z
-        results = requests.post('http://www2.cookcountysheriff.org/search2/locatesearchresults.asp',
-                data={'LastName': 'A', 'FirstName': '', 'Submit': 'Begin Search'})
-        document = pq(results.content)
-        rows = document('#mainContent table tr')
-        # Loop over rows to extract URL for record
-        # Follow link
-        # get_or_create new inmate record
+        for search in string.uppercase[:26]:
+            results = requests.post( "%s/%s" % (BASE_URL, 'locatesearchresults.asp'),
+                    data={'LastName': search, 'FirstName': '', 'Submit': 'Begin Search'})
+            
+            # Create pquery object
+            document = pq(results.content)
+
+            # Get links from last column of each row
+            inmate_urls = document('#mainContent table tr td:last-child a')
+
+            # Get each link
+            for url in inmate_urls:
+                inmate_result = requests.get("%s/%s" % (BASE_URL, url.attrib['href']))
+                inmate_doc = pq(inmate_result.content)
+                inmate_columns = inmate_doc('table tr:last-child td')
+
+                # Jail ID is first td
+                jail_id = inmate_columns[0].text_content().strip()
+
+                # Last name is in second td, before comma
+                last_name = inmate_columns[1].text_content().strip().split(',')[0]
+
+                # First name and initials are in second td, after comma
+                first_name = inmate_columns[1].text_content().strip().split(',')[1]
+
+                # Get or create inmate based on jail_id
+                inmate, created = CountyInmate.objects.get_or_create(jail_id=jail_id)
+
+                # Set processed inmate fields
+                inmate.last_name = last_name
+                inmate.first_name = first_name
+                # @TODO Generalize to handle all fields
+
+                inmate.save()
--- countyapi/models.py
+++ countyapi/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class CountyInmate(models.Model):
-    jail_id=models.IntegerField(primary_key=True)
+    jail_id=models.CharField(max_length=12, primary_key=True)
     last_name=models.CharField(max_length=100)
+    first_name=models.CharField(max_length=100)
     # @TODO Add other fields
     # See e.g. http://www2.cookcountysheriff.org/search2/details.asp?jailnumber=2012-0813098 
