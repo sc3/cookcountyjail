a553cf6b57dbb5c45bcc09eb65e8b4f84209e451
David Eads <davideads@gmail.com>
2012-11-21 16:38:35 -0600

    Update inmate model with new url field, add silly summary table as example.

--- countyapi/models.py
+++ countyapi/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 class CountyInmate(models.Model):
-    jail_id=models.CharField(max_length=16, primary_key=True)
+    jail_id=models.CharField(max_length=64, primary_key=True)
+    url=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     first_name=models.CharField(max_length=255)
     # @TODO Add other fields
     # See e.g. http://www2.cookcountysheriff.org/search2/details.asp?jailnumber=2012-0813098 
 
     def __unicode__(self):
-        return "%s, %s (%s)" % (self.last_name, self.first_name, self.jail_id)
+        if self.last_name and self.first_name:
+            return "%s - %s, %s" % (self.jail_id, self.last_name, self.first_name)
+        return self.jail_id
+
+class InmateRecordCount(models.Model):
+    """ This is a silly example. @TODO Create real summaries. """
+    date = models.DateTimeField(auto_now_add=True)
+    record_count = models.IntegerField()
