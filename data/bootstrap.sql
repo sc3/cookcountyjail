b285417a4d63256174f93e93ce5ebbf9ea8c1860
David Eads <davideads@gmail.com>
2012-11-21 17:55:12 -0600

    add bootstrap.sql

---
+++ data/bootstrap.sql
@@ -0,0 +1,3 @@
+DROP DATABASE cookcountyjail;
+CREATE DATABASE cookcountyjail CHARACTER SET utf8;
+GRANT ALL PRIVILEGES ON cookcountyjail.* to cookcountyjail@'localhost' identified by '$9jcn1ma^5lY';
