631aa60341de075aa8d4ea6089318bcf7f12d264
David Eads <davideads@gmail.com>
2012-12-12 23:17:09 -0600

    Split court date and location into separate models to provide chronology of court dates. Requires db reset. Fixes #18.

--- countyapi/models.py
+++ countyapi/models.py
@@ -12,19 +12,25 @@ class CountyInmate(models.Model):
     height=models.IntegerField(null=True, blank=True)
     weight=models.IntegerField(null=True, blank=True)
     age_at_booking=models.IntegerField(null=True, blank=True)
-    next_court_date=models.DateField(null=True)
-    next_court_location=models.TextField(null=True)
     bail_status=models.CharField(max_length=50, null=True)
     bail_amount=models.IntegerField(null=True, blank=True)
     housing_location=models.CharField(null=True,max_length=20)
     charges=models.TextField(null=True)
     charges_citation=models.TextField(null=True)
-    
-    # @TODO Add other fields
-    # See e.g. http://www2.cookcountysheriff.org/search2/details.asp?jailnumber=2012-0813098 
 
     def __unicode__(self):
         return self.jail_id
+
+
+class CourtDate(models.Model):
+    inmate=models.ForeignKey('CountyInmate', related_name="court_dates")
+    location=models.ForeignKey('CourtLocation')
+    date=models.DateField()
+
+
+class CourtLocation(models.Model):
+    location=models.TextField()
+
 
 class InmateRecordCount(models.Model):
     """ This is a silly example. @TODO Create real summaries. """
