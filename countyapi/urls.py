bf1b8fc94fb70254b2fa52b847261e553a092a56
David Eads <davideads@gmail.com>
2012-12-30 16:36:51 -0600

    register court date and location resources with tastypie

--- countyapi/urls.py
+++ countyapi/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from countyapi.api.resources import CountyInmateResource
+from countyapi.api.resources import CountyInmateResource, CourtDateResource, CourtLocationResource
 
 v1_api = Api(api_name='1.0')
 v1_api.register(CountyInmateResource())  
+v1_api.register(CourtDateResource())  
+v1_api.register(CourtLocationResource())  
 
 urlpatterns = patterns('',
-    
     url(r'', include(v1_api.urls)),
 )
