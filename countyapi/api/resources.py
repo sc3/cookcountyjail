f1e7c3e1346f73c09837fe7e119e41440757bb30
David Eads <davideads@gmail.com>
2013-01-04 01:53:22 -0600

    Significant API tweaks to handle relationships.

--- countyapi/api/resources.py
+++ countyapi/api/resources.py
@@ -1,4 +1,4 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from tastypie import fields
 from countyapi.models import CountyInmate, CourtLocation, CourtDate
 
@@ -6,38 +6,74 @@ class CourtLocationResource(ModelResource):
     class Meta:
         queryset = CourtLocation.objects.all()
         allowed_methods = ['get']
+        include_resource_uri = False
 
-class CourtLocationHistoryResource(ModelResource):
-    court_dates = fields.ToManyField('countyapi.api.resources.CourtDateResource', 'court_dates', full=True)
-    class Meta:
-        limit = 5
-        queryset = CourtLocation.objects.all()
+    def dehydrate(self, bundle):
+        # Show court dates in location lists and detail views
+        if bundle.request.path.startswith("/api/1.0/courtlocation/") and not bundle.request.REQUEST.get('compact'):
+            dates = bundle.obj.court_dates.all()
+            resource = CourtDateResource()
+            bundle.data["court_dates"] = []
+            for court_date in dates:
+                date_bundle = resource.build_bundle(obj=court_date, request=bundle.request)
+                bundle.data["court_dates"].append(resource.full_dehydrate(date_bundle).data)
+        return bundle
 
-class CourtDateResource(ModelResource):
-    location = fields.ForeignKey('countyapi.api.resources.CourtLocationResource', 'location', full=True)
-    class Meta:
-        queryset = CourtDate.objects.all()
-        allowed_methods = ['get']
-    
 class CountyInmateResource(ModelResource):
-    court_dates = fields.ToManyField('countyapi.api.resources.CourtDateResource', 'court_dates', full=True)
     class Meta:
         queryset = CountyInmate.objects.all()
         allowed_methods = ['get']
+        include_resource_uri = False
         limit = 100
+
+        # Exclude non-essential data. Reintroduce to API if needed.
+        excludes = ['height', 'weight', 'last_seen_date', 'discharge_date_latest', 'race', 'url']
+
         filtering = {
-            'jail_id': ['exact'],
-            'race': ['exact'],
-            'last_seen_date': ['exact', 'lte', 'lt', 'gte', 'gt'],
-            'booking_date': ['exact', 'lte', 'lt',  'gte', 'gt'],
-            'discharge_date_earliest': ['exact', 'lte', 'lt', 'gte', 'gt'],
-            'discharge_date_latest': ['exact', 'lte', 'lt', 'gte', 'gt'],
-            'gender': ['exact'],
-            'height': ['lte', 'lt', 'gte', 'gt'],
-            'weight': ['lte', 'lt', 'gte', 'gt'],
-            'age_at_booking': ['exact', 'lte', 'lt', 'gte', 'gt'],
-            'bail_amount': ['lte', 'lt', 'gte', 'gt'],
-            'housing_location': ['exact'],
-            'charges': ['exact'],
-            'charges_citation': ['exact'],
+            'jail_id': ALL,
+            'booking_date': ALL,
+            'discharge_date_earliest': ALL,
+            'gender': ALL,
+            'age_at_booking': ALL,
+            'bail_amount': ALL,
+            'housing_location': ALL,
+            'charges': ALL,
+            'charges_citation':ALL,
         }
+
+    def dehydrate(self, bundle):
+        # Show court dates in inmate lists and detail views
+        if bundle.request.path.startswith("/api/1.0/countyinmate/") and not bundle.request.REQUEST.get('compact'):
+            dates = bundle.obj.court_dates.all()
+            resource = CourtDateResource()
+            bundle.data["court_dates"] = []
+            for court_date in dates:
+                date_bundle = resource.build_bundle(obj=court_date, request=bundle.request)
+                bundle.data["court_dates"].append(resource.full_dehydrate(date_bundle).data)
+        return bundle
+
+
+class CourtDateResource(ModelResource):
+    class Meta:
+        queryset = CourtDate.objects.all()
+        allowed_methods = ['get']
+        include_resource_uri = False
+
+    def dehydrate(self, bundle):
+        # Include inmate when called from location
+        if bundle.request.path.startswith("/api/1.0/courtlocation/"):
+            inmate = bundle.obj.inmate
+            resource = CountyInmateResource()
+            inmate_bundle = resource.build_bundle(obj=inmate, request=bundle.request)
+            bundle.data["inmate"] = resource.full_dehydrate(inmate_bundle).data
+
+        # Include location when called from inmate
+        if bundle.request.path.startswith("/api/1.0/countyinmate/"):
+            location = bundle.obj.location
+            resource = CourtLocationResource()
+            location_bundle = resource.build_bundle(obj=location, request=bundle.request)
+            bundle.data["location"] = resource.full_dehydrate(location_bundle).data
+
+        return bundle
+
+
--- countyapi/urls.py
+++ countyapi/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from countyapi.api.resources import CountyInmateResource, CourtDateResource, CourtLocationResource, CourtLocationHistoryResource
+from countyapi.api.resources import CountyInmateResource, CourtLocationResource
 
 v1_api = Api(api_name='1.0')
 v1_api.register(CountyInmateResource())  
-v1_api.register(CourtDateResource())  
 v1_api.register(CourtLocationResource())  
-v1_api.register(CourtLocationHistoryResource())  
 
 urlpatterns = patterns('',
     url(r'', include(v1_api.urls)),
