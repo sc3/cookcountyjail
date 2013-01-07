e0472e71aa67df4490bc2c7b489b49e76cf9856a
David Eads <davideads@gmail.com>
2013-01-06 20:28:00 -0600

    cleanup

--- countyapi/api.py
+++ countyapi/api.py
@@ -1,19 +1,33 @@
 from tastypie.resources import ModelResource, ALL
 from tastypie import fields
 from countyapi.models import CountyInmate, CourtLocation, CourtDate
+from django.core.serializers import json
+from django.utils import simplejson
+from tastypie.serializers import Serializer
+
+class PrettyJSONSerializer(Serializer):
+    json_indent = 2
+
+    def to_json(self, data, options=None):
+        options = options or {}
+        data = self.to_simple(data, options)
+        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
+                sort_keys=True, ensure_ascii=False, indent=self.json_indent)
+
 
 class CourtLocationResource(ModelResource):
     class Meta:
         queryset = CourtLocation.objects.all()
         allowed_methods = ['get']
         include_resource_uri = False
+        serializer = PrettyJSONSerializer()
 
     def dehydrate(self, bundle):
         # Show court dates in location lists and detail views
-        if bundle.request.path.startswith("/api/1.0/courtlocation/") and not bundle.request.REQUEST.get('compact'):
+        if bundle.request.path == '/api/1.0/courtlocation/' and bundle.request.REQUEST.get('related'):
             dates = bundle.obj.court_dates.all()
             resource = CourtDateResource()
-            bundle.data["court_dates"] = []
+            bundle.data['court_dates'] = []
             for court_date in dates:
                 date_bundle = resource.build_bundle(obj=court_date, request=bundle.request)
                 bundle.data["court_dates"].append(resource.full_dehydrate(date_bundle).data)
@@ -25,6 +39,7 @@ class CountyInmateResource(ModelResource):
         allowed_methods = ['get']
         include_resource_uri = False
         limit = 100
+        serializer = PrettyJSONSerializer()
 
         # Exclude non-essential data. Reintroduce to API if needed.
         excludes = ['height', 'weight', 'last_seen_date', 'discharge_date_latest', 'url']
@@ -44,10 +59,10 @@ class CountyInmateResource(ModelResource):
 
     def dehydrate(self, bundle):
         # Show court dates in inmate lists and detail views
-        if bundle.request.path.startswith("/api/1.0/countyinmate/") and not bundle.request.REQUEST.get('compact'):
+        if bundle.request.path == '/api/1.0/countyinmate/' and bundle.request.REQUEST.get('related'):
             dates = bundle.obj.court_dates.all()
             resource = CourtDateResource()
-            bundle.data["court_dates"] = []
+            bundle.data['court_dates'] = []
             for court_date in dates:
                 date_bundle = resource.build_bundle(obj=court_date, request=bundle.request)
                 bundle.data["court_dates"].append(resource.full_dehydrate(date_bundle).data)
--- countyapi/urls.py
+++ countyapi/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from countyapi.api.resources import CountyInmateResource, CourtLocationResource
+from countyapi.api import CountyInmateResource, CourtLocationResource
 
 v1_api = Api(api_name='1.0')
 v1_api.register(CountyInmateResource())  
