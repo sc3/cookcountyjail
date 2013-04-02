2b8613bc76ae3756f82a9e9449abc96e9149b3c9
David Eads <davideads@gmail.com>
2013-04-02 00:04:38 -0500

    restart nginx to take advantage of long cache time

--- cron.sh
+++ cron.sh
@@ -3,3 +3,5 @@ echo "Cook County Jail scraper started at `date`"
 source /home/ubuntu/.virtualenvs/cookcountyjail/bin/activate
 python /home/ubuntu/apps/cookcountyjail/manage.py scrape_inmates -d
 echo "Cook County Jail scraper finished at `date`"
+echo "Restarting nginx at `date`"
+sudo service nginx restart
