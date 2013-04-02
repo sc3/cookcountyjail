a44ca640334bd7438c00d2d63f217a9f96b01488
David Eads <davideads@gmail.com>
2013-04-02 05:31:53 +0000

    tweak settings, fix cron script

--- cron.sh
+++ cron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export CCJ_PRODUCTION=1
 echo "Cook County Jail scraper started at `date`"
 source /home/ubuntu/.virtualenvs/cookcountyjail/bin/activate
 python /home/ubuntu/apps/cookcountyjail/manage.py scrape_inmates -d
