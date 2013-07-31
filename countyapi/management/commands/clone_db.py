6ecd22b3efc588fb2f9bf1184733a5ef851dbeab
Brian Peterson <bepetersn@gmail.com>
2013-07-31 05:38:25 +0000

    added limit and api options; expanded comments, formatted

--- countyapi/management/commands/clone_db.py
+++ countyapi/management/commands/clone_db.py
@@ -10,6 +10,14 @@ import collections
 
 class Command(BaseCommand):
 
+    help = "Copies all data from the CookCountyJail database."
+    option_list = BaseCommand.option_list + (
+        make_option('-a', '--api', type='string', action='store', dest='api', default=False,
+                    help='APIs to clone by name, separated by comma'), 
+        make_option('-l', '--limit', type='string', action='store', dest='limit', default=False,
+                    help='take only the first n records')
+        )
+
     def handle(self, *args, **options):
 
         """
@@ -19,11 +27,12 @@ class Command(BaseCommand):
 
         base_url = "http://cookcountyjail.recoveredfactory.net/api/1.0/"
         suffix = "?format=json&limit=0"
-
+        exclude = []  
+        
         # ordereddict mapping APIs with:
         # --> a Django model that can be used to create an object; and,
         # --> a set of identifying filters needed to initialize that object
-        api_to_model_and_id = collections.OrderedDict([
+        api_map = collections.OrderedDict([
             ('countyinmate', (CountyInmate, ['jail_id'])),
             ('courtlocation', (CourtLocation, ['location'])),
             ('housinglocation', (HousingLocation, ['housing_location'])),
@@ -37,20 +46,35 @@ class Command(BaseCommand):
         #     the object we'll create with our foreign model
         # --> an API to use to get a model and initializing filters;
         #     these will be used to create the foreign object 
-        id_to_jsonkey_and_api = {
+        foreign_map = {
             'inmate' : ('inmate_jail_id', 'countyinmate'),
             'location' : ('location', 'courtlocation'),
             'housing_location' : ('location_id', 'housinglocation')
         }
 
+         # set a limit if necessary
+        if options['limit']:
+            suffix = '?format=json&limit=%s' % options['limit']
+
+
+        # build a list of excluded APIs if necessary
+        if options['api']:
+            for k in api_map.keys():
+                if k not in options['api'].split(', '):
+                    exclude.append(k)      
+
         # iterate over our APIs
-        for api in api_to_model_and_id.keys():
+        for api in api_map.keys():
+
+            # if '--api' flag was given, but this API wasn't included, ignore it.
+            if api in exclude:
+                continue
 
             # for each API, get the model we'll use to re-create all of its data            
-            model = api_to_model_and_id[api][0]
+            model = api_map[api][0]
 
             # for each API, get the identifying filters we'll need to initialize each object of this type
-            identifiers = api_to_model_and_id[api][1]
+            identifiers = api_map[api][1]
 
             # query our server to get all the data of a particular API
             result = http_get("%s%s%s" % (base_url, api, suffix), number_attempts=1, quiet=False)
@@ -63,8 +87,8 @@ class Command(BaseCommand):
                 for json_obj in json_results['objects']:
 
                     # this a dict of filters we'll use to initialize our object, when we create it.
-                    # if we have foreign keys to deal with, getting the value for each filter
-                    # is a bit more complex.
+                    # if we have foreign keys to deal with, we'll have to first get_or_create
+                    # an object based on those foreign keys.
                     filters = {}
 
                     # but if there's only one filter, the model doesn't have any foreign keys,
@@ -76,29 +100,38 @@ class Command(BaseCommand):
                         filters = {identifiers[0]: json_obj[identifiers[0]]}
                     else:
 
-                        # iterate through the filters
+                        # otherwise, iterate through the filters
                         for i in identifiers:
 
                             # if we have defined an attribute as requiring foreignkey
-                            # lookup, we get to work. Continuing from the example above,
-                            # this IS the case for the 'inmate' and 'location' identifiers
-                            if i in id_to_jsonkey_and_api.keys():
-                                json_key = id_to_jsonkey_and_api[i][0]
-                                foreign_api = id_to_jsonkey_and_api[i][1]
-                                foreign_model = api_to_model_and_id[foreign_api][0]
-                                foreign_key = api_to_model_and_id[foreign_api][1][0]
+                            # lookup, we get to work. 
+                            if i in foreign_map.keys():
+                                json_key = foreign_map[i][0]
+                                foreign_api = foreign_map[i][1]
+                                foreign_model = api_map[foreign_api][0]
+                                foreign_key = api_map[foreign_api][1][0]
+
+                                # do a foreign key lookup; this means doing get_or_create to instantiate
+                                # the object before including it as an attribute of our object;
+                                # note that we take the first match if there are multiple
                                 try:
-                                    foreigner, created = foreign_model.objects.get_or_create(**{foreign_key: json_obj[json_key]})
+                                    foreigner, created = foreign_model.objects.get_or_create(
+                                            **{foreign_key: json_obj[json_key]})
                                 except MultipleObjectsReturned:
-                                    foreigner = foreign_model.objects.filter(**{foreign_key: json_obj[json_key]})[0]
+                                    foreigner = foreign_model.objects.filter(
+                                            **{foreign_key: json_obj[json_key]})[0]
+
+                                # include the result with our list of filters
                                 filters[i] = foreigner
+
                             else:
 
-                                # but the 'date' attribute isn't defined in our map, 
-                                # so it would get pulled from the json object like normal.
+                                # if an attribute isn't defined in our map above as a foreign key, 
+                                # it would get pulled from the json object like normal. 
                                 filters[i] = json_obj[i]
 
-                    # here, we actually create our object.
+                    # here, we actually create our object, again taking the first
+                    # available, if there are multiple
                     try:
                         obj, created = model.objects.get_or_create(**filters)
                     except MultipleObjectsReturned: 
@@ -112,7 +145,8 @@ class Command(BaseCommand):
                         # if the value is None, it stays None; 
                         # if we already assigned the key in filters, don't reassign; 
                         # if key is not part of our model, don't bother assigning it (e.g. 'resource_uri', '_state')
-                       if k not in filters.keys() and v is not None and k in [a for a in obj.__dict__ if not a.startswith('_')]:
+                       if k not in filters.keys() and v is not None and \
+                                k in [a for a in obj.__dict__ if not a.startswith('_')]:
                             setattr(obj, k, v)
 
                     # finally, we're done.
