b823eca753b2d232d80bd79dd34c3b07f31abb4c
wilbertom <wilbertomorales777@gmail.com>
2013-08-04 05:37:58 -0500

    robots.txt file that allows all bots to scrape close #92

--- cookcountyjail/urls.py
+++ cookcountyjail/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.simple import direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r"^$", direct_to_template, {"template": "index.html"}),
+                       url(r'^$', direct_to_template, {'template': 'index.html'}),
+                       url(r'^robots.txt', direct_to_template, {'template': 'robots.txt', 'mimetype':'text/plain'}),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api/', include('countyapi.urls')))
