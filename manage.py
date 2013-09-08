85b0e37fa0905cdb1130e7cf76c88bee38562355
Wilberto Morales <wilbertomorales777@gmail.com>
2013-09-08 05:29:40 -0500

    Base Person Model and echo sql command arg

--- manage.py
+++ manage.py
@@ -4,9 +4,13 @@ from scripts import *
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-sdb', '--setupdb', action='store_true', help='Creates all the database models')
+    parser.add_argument('-sdb', '--setupdb', action='store_true', help='Creates all the database models.')
+    parser.add_argument('--loudsql', action='store_true', help='Logs all sql executed. Good to double check your queries.')
 
     args = parser.parse_args()
+
+    if args.loudsql:
+        app.config['SQLALCHEMY_ECHO'] = True
 
     if args.setupdb:
         setup_db(db)
