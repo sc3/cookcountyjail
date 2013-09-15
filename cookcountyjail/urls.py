5a52f1397e79f89115cad4b8dc426bc897ba4a6d
Brian Peterson <bepetersn@gmail.com>
2013-09-14 23:35:37 -0500

    simplified routing scheme for handling just the API.

--- cookcountyjail/urls.py
+++ cookcountyjail/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
-
 urlpatterns = patterns('',
-                       url(r'^api/', include('countyapi.urls')),
-                       url(r'^favicon\.ico', direct_to_template, {'template': 'favicon.ico',
-                                                                  'mimetype': 'image/vnd.microsoft.icon'}),
-                       url(r'^$|^index\.html$', direct_to_template, {'template': 'index.html'}),
-                       url(r'^robots\.txt', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-                       url(r'^admin/', include(admin.site.urls))
-                       )
+                       url(r'^api/', include('countyapi.urls')))
