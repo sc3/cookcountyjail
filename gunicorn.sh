8dee2d0647ee9857f9be6e88efcb67e94e5d98c7
Brian Peterson <bepetersn@gmail.com>
2013-10-09 18:16:17 -0500

    updated gunicorn script to match syntax for django 1.5.4 update

--- gunicorn.sh
+++ gunicorn.sh
@@ -12,6 +12,6 @@ USER=ubuntu
 GROUP=ubuntu
 cd $PROJECT_DIR
 source /home/ubuntu/.virtualenvs/cookcountyjail/bin/activate
-exec gunicorn_django -w $NUM_WORKERS \
+gunicorn cookcountyjail.wsgi:application -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --log-level=debug \
     --timeout=$TIMEOUT --log-file=$LOGFILE 2>>$LOGFILE
