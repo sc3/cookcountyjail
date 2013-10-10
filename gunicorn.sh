ff45463e98263324f0efb293b39bbe973ef0c106
Brian Peterson <bepetersn@gmail.com>
2013-10-09 22:00:35 -0500

    made a stronger gunicorn.sh for v2.0 modeled after 1.0

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,17 @@
-# Run your virtualenv's version of gunicorn, with 4 workers, 
-# on port 8000 of your localhost, using a Python WSGI callable 
-# located underneath ccj in app.py, and called app
-gunicorn -w 4 -b 127.0.0.1:8080 ccj.app:app 
-
-
+#!/bin/bash
+set -e
+PROJECT_DIR=/home/ubuntu/repos/cookcountyjail_2.0-dev
+LOGFILE=/home/ubuntu/logs/cookcountyjail-v2.0.log
+LOGDIR=$(dirname $LOGFILE)
+export CCJ_PRODUCTION=1
+export CACHE_TTL=86400
+NUM_WORKERS=4
+TIMEOUT=240
+USER=ubuntu
+GROUP=ubuntu
+cd $PROJECT_DIR
+source /home/ubuntu/.virtualenvs/cookcountyjail_2.0-dev/bin/activate
+gunicorn -w $NUM_WORKERS -b 127.0.0.1:8080 \
+ --log-level=debug --user=$USER --group=$GROUP \
+ --timeout=$TIMEOUT --log-file=$LOGFILE \
+ ccj.app:app 2>>$LOGFILE
