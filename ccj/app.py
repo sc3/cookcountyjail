5f59c0ab5b69968bbf76a80b383ca7cf7143982c
Norbert Winklareth <norbert.winklareth@gmail.com>
2013-10-15 23:10:37 -0500

    Added route to display os environment info, first info in current working directory.

--- ccj/app.py
+++ ccj/app.py
@@ -6,10 +6,24 @@ app = Flask(__name__, static_url_path='')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 db = SQLAlchemy(app)
 
+import os
+
 
 @app.route('/version')
-def temp_api():
+def version_info():
     """
     Temporary api return.
     """
-    return jsonify(Version=2.0, Build=777, Deployed='2013-10-15')
+    return jsonify(Version="2.0",
+                   Build=777,
+                   Deployed='2013-10-15')
+
+
+@app.route('/os_env_info')
+def env_info():
+    """
+    Displays information about the current OS environment.
+    Used for development purposes, to be deleted when this is no longer a dev branch.
+    """
+    return jsonify(cwd=os.getcwd()
+                   )
