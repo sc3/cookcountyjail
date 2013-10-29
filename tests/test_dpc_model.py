f0aeda42e8df916e13f5f9814eeaa153f6cebd0d
Brian Peterson <bepetersn@gmail.com>
2013-10-29 01:44:09 -0500

    added back in dpc model test with multiple data

--- tests/test_dpc_model.py
+++ tests/test_dpc_model.py
@@ -18,26 +18,43 @@ class Test_DailyPopulationChanges_Model:
         assert self.dpc.query() == []
 
     def test_one_data_should_return_array_with_data(self):
-        expected = {
+        expected = [{
             'Date': '2013-10-18',
             'Booked': {
-                'Male': {'AS': randint(0, 101)}
+                'Male': {'As': str(randint(0, 101))}
             }
-        }
-        data = self.dpc._expected_to_storage(expected)
+        }]
+        data = self.dpc._format_expected(expected[0])
         self.dpc.store(data)
-        assert self.dpc.query() == [expected]
+        assert self.dpc.query() == expected
 
-    def test_one_data_should_match_json(self):
-        expected = {
+    def test_multiple_data_should_return_array_with_data(self):
+        expected = [
+        {
             'Date': '2013-10-18',
             'Booked': {
-                'Male': {'AS': randint(0, 101)}
+                'Male': {'As': str(randint(0, 101))}
+            }
+        },
+        {
+            'Date': '2013-10-19',
+            'Booked': {
+                'Male': {'As': str(randint(0, 101))}
+            }
+        },
+        {
+            'Date': '2013-10-20',
+            'Booked': {
+                'Male': {'As': str(randint(0, 101))}
             }
         }
-        data = self.dpc._expected_to_storage(expected)
-        self.dpc.store(data)
-        assert self.dpc.to_json() == dumps([expected])
+        ]
+        for entry in expected:
+            e = self.dpc._format_expected(entry)
+            self.dpc.store(e)
+        assert self.dpc.query() == expected
+
+
 
 
 ##########
@@ -48,3 +65,4 @@ class Test_DailyPopulationChanges_Model:
 
 
 from ccj.app import app
+
