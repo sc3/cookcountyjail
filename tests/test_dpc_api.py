6b59e46a9e55a2254faf6c1eaac60b65c4864d0d
Norbert Winklareth <norbert.winklareth@gmail.com>
2013-11-02 16:51:55 -0500

    Changed returned format of booked asian males to new tabular format

--- tests/test_dpc_api.py
+++ tests/test_dpc_api.py
@@ -7,7 +7,7 @@ from json import loads
 from random import randint
 
 from ccj.app import app
-from helper import flatten_dpc_dict, safe_remove_file
+from helper import safe_remove_file
 
 
 class Test_DailyPopulationChanges_API:
@@ -28,31 +28,20 @@ class Test_DailyPopulationChanges_API:
         assert result.data == expected
 
     def test_post_with_one_entry_should_store_result(self):
-        expected = [
-            {
-                'Date': '2013-10-30',
-                'Males': {
-                    'Booked': {'AS': str(randint(0, 101))}
-                }
-            }
-        ]
+        expected = {
+            'date': '2013-10-30',
+            'booked_males_as': str(randint(0, 101))
+        }
         result = self.client.post('/daily_population_changes',
-                                  data=flatten_dpc_dict(expected[0]))
+                                  data=expected,
+                                  environ_overrides={'REMOTE_ADDR': '127.0.0.1'})
         assert result.status_code == 201
 
         result = self.client.get('/daily_population_changes')
-        assert loads(result.data, encoding='ASCII') == expected
+        assert loads(result.data, encoding='ASCII') == [expected]
 
     def test_external_post_fails(self):
-
-        data = {
-            'Date': '2013-10-30',
-            'Males': {
-                'Booked': {'As': str(randint(0, 101))}
-            }
-        }
-
         result = self.client.post('/daily_population_changes',
-                                  data=flatten_dpc_dict(data),
+                                  data={},
                                   environ_overrides={'REMOTE_ADDR': '127.0.0.2'})
         assert result.status_code == 401
--- tests/test_dpc_model.py
+++ tests/test_dpc_model.py
@@ -1,7 +1,7 @@
 from random import randint
 from ccj.models.daily_population_changes \
     import DailyPopulationChanges as DPC
-from helper import flatten_dpc_dict, safe_remove_file
+from helper import safe_remove_file
 
 
 class Test_DailyPopulationChanges_Model:
@@ -18,38 +18,28 @@ class Test_DailyPopulationChanges_Model:
         assert self.dpc.query() == []
 
     def test_one_data_should_return_array_with_data(self):
-        expected = [{
-            'Date': '2013-10-18',
-            'Males': {
-                'Booked': {'AS': str(randint(0, 101))}
-            }
-        }]
-        data = flatten_dpc_dict(expected[0])
-        self.dpc.store(data)
-        assert self.dpc.query() == expected
+        expected = {
+            'date': '2013-10-18',
+            'booked_males_as': str(randint(0, 101))
+        }
+        self.dpc.store(expected)
+        assert self.dpc.query() == [expected]
 
     def test_multiple_data_should_return_array_with_data(self):
         expected = [
             {
-                'Date': '2013-10-18',
-                'Males': {
-                    'Booked': {'AS': str(randint(0, 101))}
-                }
+                'date': '2013-10-18',
+                'booked_males_as': str(randint(0, 101))
             },
             {
-                'Date': '2013-10-19',
-                'Males': {
-                    'Booked': {'AS': str(randint(0, 101))}
-                }
+                'date': '2013-10-19',
+                'booked_males_as': str(randint(0, 101))
             },
             {
-                'Date': '2013-10-20',
-                'Males': {
-                    'Booked': {'AS': str(randint(0, 101))}
-                }
+                'date': '2013-10-20',
+                'booked_males_as': str(randint(0, 101))
             }
         ]
         for entry in expected:
-            e = flatten_dpc_dict(entry)
-            self.dpc.store(e)
+            self.dpc.store(entry)
         assert self.dpc.query() == expected
