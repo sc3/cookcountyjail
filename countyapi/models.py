3dc1bf5ee994f55d195b61d24a4e57048c21bf90
Norbert Winklareth <norbert.winklareth@gmail.com>
2013-11-09 10:36:07 -0600

    Issue #213 - dropped url column from CountyInmate table

--- countyapi/models.py
+++ countyapi/models.py
@@ -7,7 +7,6 @@ class CountyInmate(models.Model):
     """
     jail_id = models.CharField(max_length=15, primary_key=True)
     person_id = models.CharField(max_length=64, null=True)
-    url = models.CharField(max_length=255)
     race = models.CharField(max_length=4, null=True, blank=True)
     last_seen_date = models.DateTimeField(auto_now=True)
     booking_date = models.DateTimeField(null=True)
