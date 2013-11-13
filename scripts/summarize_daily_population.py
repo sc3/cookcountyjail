af6bdbbdf63a619888cf436d2cc1e4da98567694
Brian Peterson <bepetersn@gmail.com>
2013-11-13 14:07:53 -0600

    separated summarize functionality into units

--- scripts/summarize_daily_population.py
+++ scripts/summarize_daily_population.py
@@ -10,19 +10,24 @@ from ccj.config import get_dpc_path
 
 
 class SummarizeDailyPopulation:
-    def _booked_males_as(self, response):
-        return len(loads(response.text)['objects'])
 
-    def date(self, date):
-        race = 'AS'
-        gender = 'M'
-        cook_county_url = 'http://cookcountyjail.recoveredfactory.net'
-        county_inmate_api = '%s/api/1.0/countyinmate' % cook_county_url
-        get_cmd = '%s?format=json&limit=0&race=%s&gender=%s&booking_date=%s' % \
-            (county_inmate_api, race, gender, date)
+    def __init__(self):
+        self.inmate_api = 'http://cookcountyjail.recoveredfactory.net/api/1.0/countyinmate?format=json&limit=1'
+
+
+    def count(self, response):
+        return loads(response.text)['meta']['total_count']
+
+
+    def fetch_count(self, date, gender, status, race):
+        get_cmd='%s&race=%s&gender=%s&booking_date__exact=%s' % \
+            (self.inmate_api, race, gender, date)
         response = requests.get(get_cmd)
         assert response.status_code == 200
+        return self.count(response)
+
+
+    def write_counts(self, data):
         dpc = DailyPopulation(get_dpc_path())
         with dpc.writer() as f:
-            f.store({'date': date,
-                     'booked_males_as': self._booked_males_as(response)})
+            f.store(data)
