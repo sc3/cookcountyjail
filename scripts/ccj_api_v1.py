b473b5cda5f0d6d21d471aeed73ca66b1acdb26b
Norbert Winklareth <norbert.winklareth@gmail.com>
2013-11-21 23:01:05 -0600

    VS2.25 - added fetching starting population data to ccj_api_v1

--- scripts/ccj_api_v1.py
+++ scripts/ccj_api_v1.py
@@ -4,12 +4,16 @@
 
 import requests
 from json import loads
+from copy import copy
 
 COOK_COUNTY_URL = 'http://cookcountyjail.recoveredfactory.net'
 COOK_COUNTY_INMATE_API = '%s/api/1.0/countyinmate' % COOK_COUNTY_URL
 BOOKING_DATE_URL_TEMPLATE = '%s?format=json&limit=0&booking_date__exact=%%s' % COOK_COUNTY_INMATE_API
 LEFT_DATE_URL_TEMPLATE = '%s?format=json&limit=0&discharge_date_earliest__exact=%%s' % COOK_COUNTY_INMATE_API
-
+NOT_DISCHARGED_URL_TEMPLATE = '%s?format=json&limit=0&booking_date__lt=%%s&discharge_date_earliest__isnull=True' %\
+    COOK_COUNTY_INMATE_API
+DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE  = \
+    '%s?format=json&limit=0&booking_date__lt=%%s&discharge_date_earliest__gte=%%s' % COOK_COUNTY_INMATE_API
 
 class CcjApiV1:
 
@@ -23,4 +27,34 @@ class CcjApiV1:
         left_inmates_cmd = LEFT_DATE_URL_TEMPLATE % date_to_fetch
         left_inmates_response = requests.get(left_inmates_cmd)
         assert left_inmates_response.status_code == 200
-        return loads(booked_inmates_response.text) + loads(left_inmates_response.text)
+        return self._parseJSON(loads(booked_inmates_response.text) + loads(left_inmates_response.text))
+
+    def _parseJSON(self, obj):
+        if isinstance(obj, dict):
+            newobj = {}
+            for key, value in obj.iteritems():
+                key = str(key)
+                newobj[key] = self._parseJSON(value)
+        elif isinstance(obj, list):
+            newobj = []
+            for value in obj:
+                newobj.append(self._parseJSON(value))
+        elif isinstance(obj, unicode):
+            newobj = str(obj)
+        else:
+            newobj = obj
+        return newobj
+
+    def start_population_data(self, starting_date):
+        not_discharged_cmd = NOT_DISCHARGED_URL_TEMPLATE % starting_date
+        not_discharged_response = requests.get(not_discharged_cmd)
+        assert not_discharged_response.status_code == 200
+        discharged_on_or_after_start_date_command = \
+            DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE % (starting_date, starting_date)
+        discharged_response = requests.get(discharged_on_or_after_start_date_command)
+        assert discharged_response.status_code == 200
+        not_discharged_response = loads(not_discharged_response.text)
+        inmates = self._parseJSON(copy(not_discharged_response['objects']))
+        discharged_response = self._parseJSON(loads(discharged_response.content))
+        inmates.extend(discharged_response['objects'])
+        return inmates
--- tests/test_ccj_v1_0.py
+++ tests/test_ccj_v1_0.py
@@ -1,10 +1,12 @@
 
 import httpretty
 from random import randint
-from helpers import STARTING_DATE
+from helpers import STARTING_DATE, discharged_null_inmate_records, discharged_on_or_after_start_date_inmate_records,\
+     count_population, expected_starting_population
 from json import dumps
 
-from scripts.ccj_api_v1 import CcjApiV1, BOOKING_DATE_URL_TEMPLATE, LEFT_DATE_URL_TEMPLATE, COOK_COUNTY_INMATE_API
+from scripts.ccj_api_v1 import CcjApiV1, BOOKING_DATE_URL_TEMPLATE, LEFT_DATE_URL_TEMPLATE, COOK_COUNTY_INMATE_API, \
+    NOT_DISCHARGED_URL_TEMPLATE, DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE
 
 
 class Test_CcjV1:
@@ -35,4 +37,44 @@ class Test_CcjV1:
         booked_left = CcjApiV1().booked_left(STARTING_DATE)
 
         assert ccj_api_requests['booked_cmd'] and ccj_api_requests['left_cmd']
+        assert booked_left == expected
+
+    @httpretty.activate
+    def test_build_starting_population_count(self):
+        not_discharged_command = NOT_DISCHARGED_URL_TEMPLATE % STARTING_DATE
+        discharged_after_start_date_command = \
+            DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE % (STARTING_DATE, STARTING_DATE)
+        book_not_null_inmate_records = discharged_null_inmate_records(randint(15, 31))
+        discharged_on_or_after_start_date_records = \
+            discharged_on_or_after_start_date_inmate_records(randint(21, 37))
+        expected = book_not_null_inmate_records + discharged_on_or_after_start_date_records
+
+        cook_county_get_count = {}
+
+        def fulfill_county_api_request(method, uri, headers):
+            assert uri == not_discharged_command or uri == discharged_after_start_date_command
+            if uri == not_discharged_command:
+                cook_county_get_count['not_discharged_cmd'] = True
+                response = {
+                    'meta': {'total_count': len(book_not_null_inmate_records)},
+                    'objects': book_not_null_inmate_records
+                }
+            else:
+                cook_county_get_count['discharged_after_start_date_command'] = True
+                response = {
+                    'meta': {'total_count': len(discharged_on_or_after_start_date_records)},
+                    'objects': discharged_on_or_after_start_date_records
+                }
+
+            return 200, headers, dumps(response)
+
+        httpretty.register_uri(httpretty.GET, COOK_COUNTY_INMATE_API,
+                               body=fulfill_county_api_request)
+
+        booked_left = CcjApiV1().start_population_data(STARTING_DATE)
+
+        last_request = httpretty.last_request()
+        assert last_request.method == "GET"
+        assert cook_county_get_count['not_discharged_cmd'] and \
+               cook_county_get_count['discharged_after_start_date_command']
         assert booked_left == expected
