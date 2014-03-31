ba920f3800d833bb4a4f03b2f3e39f8195d89985
Brian Peterson <bepetersn@gmail.com>
2014-03-31 03:23:45 -0500

    add more tests for charges

--- tests/test_charges.py
+++ tests/test_charges.py
@@ -32,12 +32,7 @@ class TestCharges:
 
         fake_inmate_details = Mock()
         fake_inmate_details.charges.return_value = ''
-        fake_inmate_details.charges_citation.return_value = ''
 
-        fake_current_charge = Mock()
-        fake_current_charge.charges = 'DOMESTIC BTRY/PHYSICAL CONTACT'
-        fake_current_charge.charges_citation = '720 ILCS 5 12-3.2(a)(2) [10418'
-        
         fake_django_inmate = Mock()
         fake_django_inmate.charges_history.all.return_value = ['']
         fake_django_inmate.charges_history.latest.return_value = ''
@@ -48,3 +43,68 @@ class TestCharges:
         charge_under_test.save()
 
         assert not fake_django_inmate.charges_history.create.called
+
+
+    def test_same_raw_charge_has_no_result(self):
+
+        fake_inmate_details = Mock()
+        fake_inmate_details.charges.return_value = \
+                '720 ILCS 5 12-3.2(a)(2) [10418\r\n\t  DOMESTIC BTRY/PHYSICAL CONTACT'
+
+        fake_current_charge = Mock()
+        fake_current_charge.charges = 'DOMESTIC BTRY/PHYSICAL CONTACT'
+        fake_current_charge.charges_citation = '720 ILCS 5 12-3.2(a)(2) [10418'
+
+        fake_django_inmate = Mock()
+        fake_django_inmate.charges_history.all.return_value = [fake_current_charge]
+        fake_django_inmate.charges_history.latest.return_value = fake_current_charge
+
+        fake_monitor = Mock()
+
+        charge_under_test = Charges(fake_django_inmate, fake_inmate_details, fake_monitor)
+        charge_under_test.save()
+        
+        assert not fake_django_inmate.charges_history.create.called
+
+
+    def test_no_past_charge_results_in_new_charge(self):
+
+        fake_inmate_details = Mock()
+        fake_inmate_details.charges.return_value = \
+                '720 ILCS 5 12-3.2(a)(2) [10418\r\n\t  DOMESTIC BTRY/PHYSICAL CONTACT'
+
+        fake_django_inmate = Mock()
+        fake_django_inmate.charges_history.all.return_value = []
+
+        fake_monitor = Mock()        
+
+        charge_under_test = Charges(fake_django_inmate, fake_inmate_details, fake_monitor)
+        charge_under_test.save()
+
+        assert fake_django_inmate.charges_history.create.called
+
+
+    def test_different_raw_charge_results_in_new_charge(self):
+
+        fake_inmate_details = Mock()
+        fake_inmate_details.charges.return_value = \
+                '720 ILCS 5 12-3.2(a)(2) [10418\r\n\t  DOMESTIC BTRY/PHYSICAL CONTACT'
+
+        fake_current_charge = Mock()
+        fake_current_charge.charges = 'THEFT CONTROL INTENT'
+        fake_current_charge.charges_citation = '720 ILCS 5 16-1(a)(1)(A) [1114'
+        
+        fake_django_inmate = Mock()
+        fake_django_inmate.charges_history.all.return_value = [fake_current_charge]
+        fake_django_inmate.charges_history.latest.return_value = fake_current_charge
+
+        fake_monitor = Mock()
+
+        charge_under_test = Charges(fake_django_inmate, fake_inmate_details, fake_monitor)
+        charge_under_test.save()
+
+        fake_django_inmate.charges_history.create.assert_called_with(
+            charges='DOMESTIC BTRY/PHYSICAL CONTACT', 
+            charges_citation='720 ILCS 5 12-3.2(a)(2) [10418')
+
+
