b2ab61c5ab9f0277960c9eba32258bd45020e012
wilbertom <wilbertomorales777@gmail.com>
2014-05-02 01:06:52 -0500

    Migrate the other routes to flask-restful.

--- ccj/app.py
+++ ccj/app.py
@@ -7,7 +7,6 @@ from flask import Flask, jsonify, request, Response
 from flask.json import dumps
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import restful as rest
-
 from os import getcwd
 from datetime import datetime
 
@@ -15,61 +14,84 @@ from ccj.models.daily_population import DailyPopulation as DPC
 from ccj.models.version_info import VersionInfo
 from ccj import config
 
-
 STARTUP_TIME = datetime.now()
 
+class CcjApi(rest.Api):
+    """
+    An Api subclass of flask-restful's Api, because
+    we will do a lot of custom stuff.
+    """
+    def create_resource(self, get_fun, route):
+        """
+        This magical method accepts a function and
+        a route. After using this method, the
+        app's given route will respond with the
+        function. Only use this for routes that only
+        respond to GET requests.
+
+        Returns the flask.ext.restful.Resource object.
+
+        """
+        resource = simple_resource(get_fun.func_name, get_fun)
+        self.add_resource(resource, route)
+        return resource
+
+def simple_resource(class_name, get_fun):
+    """
+    Returns a Resource subclass(to be added to the API),
+    which responds to just Get requests with get_fun.
+
+    """
+    return type(class_name, (rest.Resource,), {'get': get_fun})
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 db = SQLAlchemy(app)
-
-api = rest.Api(app)
+api = CcjApi(app)
 
 if app.config['IN_TESTING']:
     app.debug = True
 
-@app.route('/daily_population', methods=['GET'])
-def daily_population():
-    """
-    returns the set of summarized daily population changes.
-    """
-    return Response(DPC(app.config['DPC_DIR_PATH']).to_json(),
-        headers={'Access-Control-Allow-Origin': '*'},
-	    mimetype='application/json')
-
-
-@app.route('/os_env_info')
-def env_info():
+def env_info(self):
     """
     Displays information about the current OS environment.
-    Used for development purposes, to be deleted when this is no longer a dev branch.
-    """
-    r_val = jsonify(
-        cwd=getcwd(),
-        remote_addr=request.environ.get('REMOTE_ADDR', 'not set'),
-        headers=str(request.headers),
-        environ=str(request.environ)
-        )
-    return r_val
+    Used for development purposes, to be deleted when this
+    is no longer a dev branch.
 
-
-@app.route('/starting_population', methods=['GET'])
-def starting_population():
     """
-    returns the set of starting daily population values used to calculate daily changes.
-    """
-    return Response(dumps(DPC(app.config['DPC_DIR_PATH']).starting_population()), mimetype='application/json')
 
-class Version(rest.Resource):
+    return jsonify(cwd=getcwd(),
+                    remote_addr=request.environ.get('REMOTE_ADDR', 'not set'),
+                    headers=str(request.headers),
+                    environ=str(request.environ))
+
+def daily_population(self):
+    """
+    returns the set of summarized daily population changes.
+
+    """
+    return DPC(app.config['DPC_DIR_PATH']).to_json()
+
+def starting_population(self):
+    """
+    returns the set of starting daily population values used
+    to calculate daily changes.
+
+    """
+    return DPC(app.config['DPC_DIR_PATH']).starting_population()
+
+def version(self):
     """
     returns the version info
+
     """
-    def get(self):
-        args = request.args
-        v_i = VersionInfo(STARTUP_TIME).fetch(all_version_info=('all' in args and args['all'] == '1'))
+    args = request.args
+    return VersionInfo(STARTUP_TIME).fetch(all_version_info=('all' in args and args['all'] == '1'))
 
-        return v_i
+EnvInfo = api.create_resource(env_info, "/os_env_info")
+DailyPopulation = api.create_resource(daily_population, "/daily_population")
+StartingPopulation = api.create_resource(starting_population, "/starting_population")
+Version = api.create_resource(version, "version")
 
-api.add_resource(Version, '/version')
 
