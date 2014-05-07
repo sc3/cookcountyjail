8d8a162b6be9716938a3b6320f3368fd50c22ec4
wilbertom <wilbertomorales777@gmail.com>
2014-05-07 10:00:10 -0500

    Use flask-script instead of argparse and add db migrations

--- manage.py
+++ manage.py
@@ -5,18 +5,14 @@ import sys
 from ccj import app, db
 from scripts import setup_db
 
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+manager = Manager(app)
+migrate = Migrate(app, db, directory='./ccj/models/migrations')
+
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-sdb', '--setupdb', action='store_true', help='Creates all the database models.')
-    parser.add_argument('--loudsql', action='store_true', help='Logs all sql executed. Good to double check your queries.')
+    manager.run()
 
-    args = parser.parse_args()
-
-    if args.loudsql:
-        app.config['SQLALCHEMY_ECHO'] = True
-
-    if args.setupdb:
-        setup_db(db)
-        sys.exit()
-
-    app.run()
