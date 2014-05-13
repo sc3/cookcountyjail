d7fcffd6775f9c3c945183b602df634a028b23c0
Norbert Winklareth <norbert.winklareth@gmail.com>
2014-05-13 17:21:38 -0500

    Issue #375 added code to scraper bash script to wait until the v1 app has restarted before terminating. This makes sure that any follow on scripts have a running scrapper to work with.

--- scripts/restart_duration_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-http_code=''
-sudo service cookcountyjail restart
-echo `date`
-while $http_code != "201";do
-  http_code=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' "http://cookcountyjail.recoveredfactory.net/api/1.0/countyinmate?format=json&limit=0&booking_date__exact=2014-03-15"`
-done
-echo `date`
