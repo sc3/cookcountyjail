29d25d295dc29c5ab15d4f3bcf931ca915348226
wilbertom <wilbertomorales777@gmail.com>
2014-05-13 19:33:01 -0500

    Ok really Add name to enum, postgres compatiblei, closes #381

--- ccj/models/models.py
+++ ccj/models/models.py
@@ -32,7 +32,7 @@ class Person(db.Model):
     hash = db.Column(db.Unicode(64), unique=True)
 
     # gender can only be M(male) or F(female)
-    gender = db.Column(db.Enum("M", "F"), name="pgender")
+    gender = db.Column(db.Enum("M", "F", name="pgender"))
 
     # race would be harder to parse with an
     # enum so we are sticking to good old
