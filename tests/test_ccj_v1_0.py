36df2814954822c42c765c9121ec0a89dce05d7f
Norbert Winklareth <norbert.winklareth@gmail.com>
2014-05-26 20:01:17 -0500

    Issue #426 - fixed broken tests

--- tests/test_ccj_v1_0.py
+++ tests/test_ccj_v1_0.py
@@ -6,7 +6,8 @@ from json import dumps
 from copy import copy
 
 from scripts.ccj_api_v1 import CcjApiV1, BOOKING_DATE_URL_TEMPLATE, LEFT_DATE_URL_TEMPLATE, COOK_COUNTY_INMATE_API, \
-    NOT_DISCHARGED_URL_TEMPLATE, DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE
+    NOT_DISCHARGED_URL_TEMPLATE, DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE, convert_to_beginning_of_day, \
+    convert_to_end_of_day
 
 
 class TestCcjV1:
@@ -17,7 +18,8 @@ class TestCcjV1:
         end_of_day = STARTING_DATE
         booked_cmd = BOOKING_DATE_URL_TEMPLATE % start_of_day
         expected_booked_value = [{'booked_val': 'booked value is %d' % randint(0, 25)}]
-        left_cmd = LEFT_DATE_URL_TEMPLATE % (start_of_day, end_of_day)
+        left_cmd = LEFT_DATE_URL_TEMPLATE % (convert_to_beginning_of_day(start_of_day),
+                                             convert_to_end_of_day(end_of_day))
         expected_left_cmd = [{'left_val': 'left value is %d' % randint(0, 25)}]
 
         expected = copy(expected_booked_value)
@@ -48,13 +50,13 @@ class TestCcjV1:
         assert booked_left == expected
 
 
-
     @httpretty.activate
     def test_build_starting_population_count(self):
-        starting_date_time = (STARTING_DATE)
-        not_discharged_command = NOT_DISCHARGED_URL_TEMPLATE % starting_date_time
-        discharged_after_start_date_command = \
-            DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE % (starting_date_time, starting_date_time)
+        starting_date = (STARTING_DATE)
+        not_discharged_command = NOT_DISCHARGED_URL_TEMPLATE % starting_date
+        discharged_on_or_after_start_date_command = \
+            DISCHARGED_ON_OR_AFTER_STARTING_DATE_URL_TEMPLATE % (starting_date,
+                                                                 convert_to_beginning_of_day(starting_date))
         book_not_null_inmate_records = discharged_null_inmate_records(randint(15, 31))
         discharged_on_or_after_start_date_records = \
             discharged_on_or_after_start_date_inmate_records(randint(21, 37))
@@ -63,7 +65,7 @@ class TestCcjV1:
         cook_county_get_count = {}
 
         def fulfill_county_api_request(_, uri, headers):
-            assert uri == not_discharged_command or uri == discharged_after_start_date_command
+            assert uri == not_discharged_command or uri == discharged_on_or_after_start_date_command
             if uri == not_discharged_command:
                 cook_county_get_count['not_discharged_cmd'] = True
                 response = {
@@ -71,7 +73,7 @@ class TestCcjV1:
                     'objects': book_not_null_inmate_records
                 }
             else:
-                cook_county_get_count['discharged_after_start_date_command'] = True
+                cook_county_get_count['discharged_on_or_after_start_date_command'] = True
                 response = {
                     'meta': {'total_count': len(discharged_on_or_after_start_date_records)},
                     'objects': discharged_on_or_after_start_date_records
@@ -87,5 +89,5 @@ class TestCcjV1:
         last_request = httpretty.last_request()
         assert last_request.method == "GET"
         assert cook_county_get_count['not_discharged_cmd'] and \
-            cook_county_get_count['discharged_after_start_date_command']
+            cook_county_get_count['discharged_on_or_after_start_date_command']
         assert booked_left == expected
